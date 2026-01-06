33f80630db3780d8cbed8de3939a7cd6a13e7566
gwl687 <475857172@qq.com>
2026-01-06 11:47:32 +0900

    01061147

--- gwlweb-server/src/main/java/com/gwl/config/FirebaseConfig.java
+++ gwlweb-server/src/main/java/com/gwl/config/FirebaseConfig.java
@@ -1,27 +1,31 @@
 package com.gwl.config;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.service-account}")
+    private String firebaseServiceAccountPath;
 
     @PostConstruct
     public void init() throws IOException {
         if (!FirebaseApp.getApps().isEmpty()) {
             return;
         }
+        InputStream serviceAccount = new FileInputStream(firebaseServiceAccountPath);
 
-        InputStream serviceAccount = new ClassPathResource("timeline-app-401b0-6a90b1a6217e.json").getInputStream();
+        // InputStream serviceAccount = new
+        // ClassPathResource("timeline-app-401b0-6a90b1a6217e.json").getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
--- gwlweb-server/src/main/java/com/gwl/service/Impl/UserServiceImpl.java
+++ gwlweb-server/src/main/java/com/gwl/service/Impl/UserServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;///
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
