4279c89ba19e1b25f45c69b617442e7b1682caa5
gwl687 <475857172@qq.com>
2026-01-10 00:31:32 +0900

    Update SecurityConfig.java

--- gwlweb-server/src/main/java/com/gwl/config/SecurityConfig.java
+++ gwlweb-server/src/main/java/com/gwl/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.gwl.config;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
